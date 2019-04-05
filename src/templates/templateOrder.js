e80f65b61f58ff761a36846a218ff2d4d9723f46
brendapsd <brenda.sd.15@gmail.com>
2019-04-04 20:08:10 -0500

    se hizo la funcionalidad de boton enviar

--- src/templates/templateOrder.js
+++ src/templates/templateOrder.js
@@ -1,42 +1,81 @@
+const mostrarOrden = () => {
+    const templatePedido = `
+                            <p>Cliente: </p>
+                        `;
+    const divPedido = document.createElement('div');
+    divPedido.innerHTML = templatePedido; 
+}; 
+
+const mostrarDesayuno = () => {
+    const templateDesayuno = `
+        <button id="cafe-americano">Café americano s/.5</button>
+        <button id="cafe-leche">Café con leche s/.7</button>
+        <button id="sandwich">Sandwich de jamón y queso s/.10</button>
+        <button id="jugo">Jugo de frutas natural s/.7</button>
+    `; 
+    const divDesayuno = document.createElement('div'); 
+    divDesayuno.innerHTML = templateDesayuno; 
+}; 
+
+const mostrarAlmuerzoCena = () => {
+    const templateAlmuerzoCena = `
+        <p>HAMBURGUESA</p>
+        <button id="hamb-simple">Hamburguesa simple s/.10</button>
+        <button id="hamb-doble">Hamburguesa doble s/.15</button>
+        <p>ACOMPAÑAMIENTOS</p>
+        <button id="papas">Papas fritas s/.5</button>
+        <button id="aros">Aros de cebolla s/.5</button>
+        <p>PARA TOMAR</p>
+        <button id="agua5">Agua 500ml s/.5</button>
+        <button id="agua7">Agua 750ml s/.7</button>
+        <button id="gaseosa5">Bebida/gaseosa 500ml s/.7</button>
+        <button id="gaseosa7">Bebida/gaseosa 750ml s/.10</button> <br> <br> <br>
+        <div id="mostrar-pedido">
+        </div>
+    `;
+    const divAlmuerzoCena = document.createElement('div');
+    divAlmuerzoCena.innerHTML = templateAlmuerzoCena; 
+}
+
 const pageOrder = () => {
     const template = `
                 <h1>BURGER QUEEN</h1>
                 <h2>Menú</h2>
-                <div>
-                    <button id="desayuno">DESAYUNO</button> <br> <br>
-                    <button id="cafe-americano">Café americano s/.5</button>
-                    <button id="cafe-leche">Café con leche s/.7</button>
-                    <button id="sandwich">Sandwich de jamón y queso s/.10</button>
-                    <button id="jugo">Jugo de frutas natural s/.7</button>
-                </div> <br>
-                <div> 
-                    <button id="almuerzo-cena">ALMUERZO Y CENA</button>
-                    <p>HAMBURGUESA</p>
-                    <button id="hamb-simple">Hamburguesa simple s/.10</button>
-                    <button id="hamb-doble">Hamburguesa doble s/.15</button>
-                    <p>ACOMPAÑAMIENTOS</p>
-                    <button id="papas">Papas fritas s/.5</button>
-                    <button id="aros">Aros de cebolla s/.5</button>
-                    <p>PARA TOMAR</p>
-                    <button id="agua5">Agua 500ml s/.5</button>
-                    <button id="agua7">Agua 750ml s/.7</button>
-                    <button id="gaseosa5">Bebida/gaseosa 500ml s/.7</button>
-                    <button id="gaseosa7">Bebida/gaseosa 750ml s/.10</button>
+                <input id="name" type="text" placeholder="Nombre del cliente">
+                <button id="btn-enviar">Enviar</button> <br> <br>
+                <button id="desayuno">DESAYUNO</button> 
+                <button id="almuerzo-cena">ALMUERZO Y CENA</button>
+                <div id="menu-desayuno">
+                </div> 
+                <div id="menu-alm-cena"> 
                 </div>
-                  `;
-    const div = document.createElement('div')
+                  `; 
+    const div = document.createElement('div'); 
     div.innerHTML = template;
-    div
-        .querySelector('#cafe-americano')
-        .addEventListener('click', () => {
-            const template1 = `<p>Café americano --> 5</p>`; 
-            const div1 = document.createElement('div')
-            return div1.innerHTML = template1; 
-        }); 
+    // const name = document.querySelector('#name').value;
+    const btnEnviar = div.querySelector('#btn-enviar');
+    const mostrarPedido = div.querySelector('#mostrar-pedido');
+    const btnDesayuno = div.querySelector('#desayuno');
+    const menuDesayuno = div.querySelector('#menu-desayuno'); 
+    const btnAlmuerzoCena = div.querySelector('#almuerzo-cena');
+    const menuAlmuerzoCena = div.querySelector('#menu-alm-cena');
+
+    btnEnviar.addEventListener('click', () => {    
+        mostrarPedido.appendChild(mostrarOrden());
+    });
+
+    btnDesayuno.addEventListener('click', () => {
+        menuDesayuno.appendChild(mostrarDesayuno());
+    });
+
+    btnAlmuerzoCena.addEventListener('click', () => {
+        menuAlmuerzoCena.appendChild(mostrarAlmuerzoCena()); 
+    });
+    
     return div
 };
 
 export const mostrar = () => {
     const contenido = document.getElementById('contenido');
     contenido.appendChild(pageOrder());
-}
+}; 
