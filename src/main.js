bc63a5d023714a42c1f7e2f9918f5d711519d744
brendapsd <brenda.sd.15@gmail.com>
2019-04-12 12:24:21 -0500

    Intentanto llamar a firestore

--- src/main.js
+++ src/main.js
@@ -15,3 +15,24 @@ const inicializa = () => {
 }
 
 window.addEventListener('load', inicializa)
+
+// var docRef = firebase.firestore().collection("menu").doc("1rASskenrjareq1EQh2X");
+
+// docRef.get().then(function(doc) {
+//     if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch(function(error) {
+//     console.log("Error getting document:", error);
+// });
+
+
+firebase.firestore().collection("menu").get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+  });
+});
