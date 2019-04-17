d19620963de99b6555928d8bb306c6674bb20a33
brendapsd <brenda.sd.15@gmail.com>
2019-04-16 22:05:59 -0500

    se muestra muestra el pedido en el resumen al hacer clic en el boton de comida

--- src/templates/templateAlmuerzoCena.js
+++ src/templates/templateAlmuerzoCena.js
@@ -1,11 +1,37 @@
 export const mostrarAlmuerzoCena = (obj) => {
     const templateAlmuerzoCena = `
-        <button id="${obj.id}" class="btn btn-warning">${obj.pedido} s/.${obj.precio}</button>
+        <button id="btn-${obj.id}" class="btn btn-warning">${obj.pedido} s/.${obj.precio}</button>
     `;
     const divAlmuerzoCena = document.createElement('div');
     divAlmuerzoCena.setAttribute('id','contenedor-almuerzo-cena')
     divAlmuerzoCena.innerHTML = templateAlmuerzoCena; 
-    console.log(divAlmuerzoCena) 
-
+    
+    const pedidoAgregado = document.getElementById('pedido-agregado')
+  
+    const btnComida = divAlmuerzoCena.querySelector(`#btn-${obj.id}`);
+    btnComida.addEventListener('click', () => {
+        console.log(`${obj.pedido}`)
+        pedidoAgregado.appendChild(agregarPedido(obj)); 
+    })
+  
     return divAlmuerzoCena
-}
+};
+
+export const agregarPedido = (obj) => {
+    const templateAgregarPedido = `
+              <td id="comida">${obj.pedido}</td>
+              <td id="precio">s/.${obj.precio}</td>
+              <td id="cantidad">
+              <i class="cursor fas fa-minus mr-2 text-secondary"></i>
+              <span class="badge badge-pill badge-primary"></span>
+              <i class="cursor fas fa-plus ml-2 text-secondary"></i>
+              </td>
+              <td id="precioXcantidad">s/.${obj.precio}</td>
+              <td id="eliminar"><button>x</button></td>
+    `; 
+    const divAgregarPedido = document.createElement('tr'); 
+    
+    divAgregarPedido.innerHTML = templateAgregarPedido; 
+  
+    return divAgregarPedido
+  }; 
--- src/templates/templateDesayuno.js
+++ src/templates/templateDesayuno.js
@@ -1,27 +1,38 @@
 export const mostrarDesayuno = (obj) => {
   const templateDesayuno = `
-        <button id="${obj.id}" class="btn btn-success">${obj.pedido} s/.${obj.precio}</button>
+        <button id="btn-${obj.id}" class="btn btn-success">${obj.pedido} s/.${obj.precio}</button>
     `; 
     
   const divDesayuno = document.createElement('div'); 
   divDesayuno.setAttribute('id', 'contenedor-desayuno');
   divDesayuno.innerHTML = templateDesayuno;
-  console.log(divDesayuno)
+
+  const pedidoAgregado = document.getElementById('pedido-agregado')
   
+  const btnComida = divDesayuno.querySelector(`#btn-${obj.id}`);
+  btnComida.addEventListener('click', () => {
+    console.log(`${obj.pedido}`)
+    pedidoAgregado.appendChild(agregarPedido(obj)); 
+  })
+
   return divDesayuno 
 }; 
 
-// export const agregarPedido = (obj) => {
-//   const templateAgregarPedido = `
-//             <td id="comida">${obj.comida}</td>
-//             <td id="precio">${obj.precio}</td>
-//             <td id="cantidad"></td>
-//             <td id="precioXcantidad">${obj.precio * cantidad}</td>
-//             <td id="eliminar"><button>x</button></td>
-//   `; 
-//   const divAgregarPedido = document.createElement('tr'); 
+export const agregarPedido = (obj) => {
+  const templateAgregarPedido = `
+            <td id="comida">${obj.pedido}</td>
+            <td id="precio">s/.${obj.precio}</td>
+            <td id="cantidad">
+            <i class="cursor fas fa-minus mr-2 text-secondary"></i>
+            <span class="badge badge-pill badge-primary"></span>
+            <i class="cursor fas fa-plus ml-2 text-secondary"></i>
+            </td>
+            <td id="precioXcantidad">s/.${obj.precio}</td>
+            <td id="eliminar"><button>x</button></td>
+  `; 
+  const divAgregarPedido = document.createElement('tr'); 
   
-//   divAgregarPedido.innerHTML = templateAgregarPedido; 
+  divAgregarPedido.innerHTML = templateAgregarPedido; 
 
-//   return divAgregarPedido
-// }
+  return divAgregarPedido
+}
